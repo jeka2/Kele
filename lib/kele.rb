b97257d3f0daf891ab970c8972af0f7fca0e4056
Yevgeniy Ivanov <anigeka@gmail.com>
2018-05-23 22:38:06 -0500

    Checkpoint complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -19,7 +19,17 @@ class Kele
   end
 
   def get_me
-    response = self.class.get('users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(id)
+    arr = []
+  ##  mentor_id = get_me["current_enrollment"]["mentor_id"] 529277
+    response = self.class.get("/mentors/#{id}/student_availability", headers: { "authorization" => @auth_token })
+    slots = JSON.parse(response.body)
+    slots.each do |x|
+      arr << x
+    end
   end
 end
